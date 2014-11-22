ef815497563957540ee0662c9bbd4e06b8205eff
Belden Lyman <belden@shutterstock.com>
2014-11-22 17:27:02 -0500

    very lame first-pass server
      curl -F file=@/tmp/foo http://localhost:3000/

---
+++ service-wordcount.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	// "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	// "regexp"
+)
+
+func wc_file(rw http.ResponseWriter, request *http.Request) {
+	// request_json, _ := json.Marshal(request)
+	// rw.Write(request_json)
+
+	file, _, err := request.FormFile("file")
+	if err == nil {
+		content, _ := ioutil.ReadAll(file)
+		fmt.Printf("got data: %s", content)
+	} else {
+		fmt.Fprintf(rw, "encoutered error: %s", err)
+	}
+
+	fmt.Fprintln(rw, "Success!")
+}
+
+func main() {
+	http.HandleFunc("/", wc_file)
+	http.ListenAndServe(":3000", nil)
+}
