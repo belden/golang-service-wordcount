c64bd6cd831379cf903ed62e7c9b049f6e42e641
Belden Lyman <belden@shutterstock.com>
2014-11-23 02:20:24 -0500

    wc_file() now takes a cache and returns an http.HandlerFunc
    Caching based on filenames - this Dictionary struct is coming
    in handy.
    
    Simplified emitting json a little bit.

--- service-wordcount.go
+++ service-wordcount.go
@@ -18,6 +18,19 @@ type Dictionary struct {
 	Words map[string]int
 }
 
+func emit_json(rw http.ResponseWriter, target Dictionary) {
+	js, err := json.Marshal(target)
+
+	rw.Header().Set("Content-Type", "application/json")
+	if err == nil {
+		rw.WriteHeader(200)
+		rw.Write(js)
+	} else {
+		rw.WriteHeader(500)
+		rw.Write([]byte(`"{\"error\": \"yikes\"}"`))
+	}
+}
+
 func count_words(words []string) Dictionary {
 
 	dict := Dictionary{Total: 0, Words: map[string]int{}}
@@ -30,33 +43,46 @@ func count_words(words []string) Dictionary {
 	return dict
 }
 
-func wc_file(rw http.ResponseWriter, request *http.Request) {
-	// for now I'll just assume it's a POST
-	file, _, err := request.FormFile("file")
+func wc_file(cache map[string]Dictionary) http.HandlerFunc {
+	// for now I'll just assume it's a POST - assuming it's within 10MB, too
+	return func(rw http.ResponseWriter, request *http.Request) {
+		file, _, err := request.FormFile("file")
 
-	// and I'm assuming the file is within the 10MB bounds
+		// grab the filename
+		request.ParseForm()
+		params := request.Form
+		fn := params["filename"]
+		filename := fn[0]
 
-	if err == nil {
-		// slurp the file, then convert byte array to a string
-		corpus_bytes, _ := ioutil.ReadAll(file)
-		corpus := string(corpus_bytes[:])
-
-		// split into an array of words, then count them
-		counts := count_words(split_words(corpus))
-
-		// emit json
-		counts_json, err := json.Marshal(counts)
-		if err == nil {
-			rw.Write(counts_json)
-		} else {
-			fmt.Fprintf(rw, "json encountered error: %s", err)
+		// bail out if it's in cache already
+		if seen, ok := cache[filename]; ok {
+			fmt.Printf("Returning cached data for %s\n", filename)
+			emit_json(rw, seen)
+			return
 		}
-	} else {
-		fmt.Fprintf(rw, "encountered error: %s", err)
+
+		if err == nil {
+			// slurp the file, then convert byte array to a string
+			corpus_bytes, _ := ioutil.ReadAll(file)
+			corpus := string(corpus_bytes[:])
+
+			// split into an array of words, then count them
+			counts := count_words(split_words(corpus))
+
+			// store in cache
+			fmt.Printf("storing data for %s in cache\n", filename)
+			cache[filename] = counts
+
+			// send response
+			emit_json(rw, counts)
+		} else {
+			fmt.Fprintf(rw, "encountered error: %s", err)
+		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/", wc_file)
+	cache := make(map[string]Dictionary)
+	http.HandleFunc("/", wc_file(cache))
 	http.ListenAndServe(":3000", nil)
 }
