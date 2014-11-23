a524fed1ca63b5b4999813abc528ad9bec4bea1c
Belden Lyman <belden@shutterstock.com>
2014-11-23 01:04:48 -0500

    basic word-frequency bit done
    - use a regex to split on wordlike characters
      be sure to include [-'] because
      "mother-in-law shouldn't visit for so long"
    	should be 6 words, not 9
    
    - use a struct to represent the output of count_words
      somehow I thought this would json-ify better, it doesn't

--- service-wordcount.go
+++ service-wordcount.go
@@ -4,17 +4,50 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	// "regexp"
+	"regexp"
 )
 
-func wc_file(rw http.ResponseWriter, request *http.Request) {
-	// request_json, _ := json.Marshal(request)
-	// rw.Write(request_json)
+func split_words(corpus string) []string {
+	words := regexp.MustCompile("[A-Za-z'-]+")
+	return words.FindAllString(corpus, -1)
+}
 
+type Dictionary struct {
+	total int
+	words map[string]int
+}
+
+func count_words(words []string) Dictionary {
+
+	dict := Dictionary{total: 0, words: map[string]int{}}
+
+	for _, word := range words {
+		dict.total++
+		dict.words[word]++
+	}
+
+	return dict
+}
+
+func wc_file(rw http.ResponseWriter, request *http.Request) {
+	// for now I'll just assume it's a POST
 	file, _, err := request.FormFile("file")
+
+	// and I'm assuming the file is within the 10MB bounds
+
 	if err == nil {
-		content, _ := ioutil.ReadAll(file)
-		fmt.Printf("got data: %s", content)
+		// slurp the file, then convert byte array to a string
+		corpus_bytes, _ := ioutil.ReadAll(file)
+		corpus := string(corpus_bytes[:])
+
+		// split into an array of words, then count them
+		counts := count_words(split_words(corpus))
+
+		// let's see what we've got here
+		fmt.Printf("total: %d\n", counts.total)
+		for word, freq := range counts.words {
+			fmt.Printf("  %s: %d\n", word, freq)
+		}
 	} else {
 		fmt.Fprintf(rw, "encoutered error: %s", err)
 	}
