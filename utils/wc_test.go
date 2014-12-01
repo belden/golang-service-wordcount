d47e059f3a8793a67e8b582e53ae346b2b50d350
Belden Lyman <belden@shutterstock.com>
2014-11-30 23:58:01 -0500

    prove Wc.CountWords works as expected
    I bet there's a testing library I could be using, but learning about "reflect"
    was neat.

--- utils/wc_test.go
+++ utils/wc_test.go
@@ -3,6 +3,7 @@ package Wc
 import (
 	"."
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -12,6 +13,33 @@ func TestSplitWords(T *testing.T) {
 	for i := range Got {
 		if Got[i] != Exp[i] {
 			T.Error(fmt.Sprintf("got %s, expected %s\n", Got[i], Exp[i]))
+		}
+	}
+}
+
+func TestCountWords(T *testing.T) {
+	Got := Wc.CountWords([]string{"hello", "world", "HELLO", "moon"})
+	Exp := Wc.Dictionary{Total: 4, Words: map[string]int{
+		"hello": 2,
+		"world": 1,
+		"moon":  1,
+	}}
+
+	// make sure Wc.CountWords hands back a Wc.Dictionary
+	GotType, ExpType := reflect.TypeOf(Got), reflect.TypeOf(Exp)
+	if GotType != ExpType {
+		T.Error(fmt.Sprintf("type mismatch: %s != %s", GotType, ExpType))
+	}
+
+	// Totals better match up
+	if Got.Total != Exp.Total {
+		T.Error(fmt.Sprintf("Total: Got %d, Expected %d\n", Got.Total, Exp.Total))
+	}
+
+	// And word frequencies better match, too
+	for ExpKey := range Exp.Words {
+		if Got.Words[ExpKey] != Exp.Words[ExpKey] {
+			T.Error(fmt.Sprintf("Words[%s]: Got %d, Expected %d\n", ExpKey, Got.Words[ExpKey], Exp.Words[ExpKey]))
 		}
 	}
 }
