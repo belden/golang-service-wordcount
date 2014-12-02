144493ee69cac8ab9bf3f2ad4202b2d52203e051
Belden Lyman <belden@shutterstock.com>
2014-12-02 10:17:21 -0500

    added a playground of stuff I'm learning
    iterate.go:
      - learning about goroutines
      - and how to represent tuples: a map[int]int seems
        simpler than `type Tuples struct { low int, high int }`
    
    this just creates an array of some size, breaks it up into
    different chunks of roughly-equivalent size, and runs a few
    goroutines against the chunks to do some marginally interesting
    work on them

---
+++ playground/iterate.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func GetFoo(Size int) []int {
+	var Out []int
+	for i := 1; i < Size+1; i++ {
+		Out = append(Out, i)
+	}
+	return Out
+}
+
+func MakeOffsets(space int, pages int) map[int]int {
+	tuples := make(map[int]int)
+
+	// MakeOffsets(10, 3) -> {0:3, 3:6, 6:10}
+	// note we handle widowing on the last page
+	widow := space % pages
+	avg_page_size := (space - widow) / pages
+	last_page_size := avg_page_size + widow
+
+	_i := 0
+	for i := 0; i <= (space - last_page_size); i += avg_page_size {
+		tuples[i] = i + avg_page_size
+		_i = i
+	}
+	tuples[_i] = space
+
+	return tuples
+}
+
+func main() {
+	Foo := GetFoo(10)
+
+	tuples := MakeOffsets(len(Foo), 3)
+
+	gotC := make(chan []int)
+
+	for low, high := range tuples {
+		low, high := low, high
+		go func() {
+			gotC <- Foo[low:high]
+		}()
+	}
+
+	var got []interface{} // the old "I don't know how to make a multidimensional array"
+	for i := 0; i < len(tuples); i++ {
+		it := <-gotC
+		got = append(got, it)
+		fmt.Printf("%d\n", it)
+	}
+
+}
